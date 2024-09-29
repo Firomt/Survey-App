670c9387221387d203534960418b9d68dbaa0f12
Firomt <firomsamt@gmail.com>
2024-09-29 23:54:47 +0300

    add vue router

--- vue/src/main.js
+++ vue/src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import './style.css'
 import store from './store'
 import './index.css'
+import router from './router'
 import App from './App.vue'
 
 
 createApp(App)
 .use(store)
+.user(router)
 .mount('#app')
---
+++ vue/src/router/index.js
@@ -0,0 +1,13 @@
+import { createRouter, createWebHashHistory, createWebHistory } from "vue-router";
+
+const routes = [];
+
+
+const router = createRouter({
+
+    history: createWebHistory(),
+    routes
+})
+
+
+export default router;
