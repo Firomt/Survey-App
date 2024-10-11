275c9f4e2f6b09f049c578e593caf027dd4b08c9
Firomt <firomsamt@gmail.com>
2024-10-11 21:17:32 +0300

    create survey form
    add form where new surveys are created and where existing surveys are edited or updated

--- vue/src/view/Surveys.vue
+++ vue/src/view/Surveys.vue
@@ -1,5 +1,39 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
   <PageComponent>
+    <template v-slot:header>
+      <div class="flex justify-between items-center">
+        <h1 class="text-3xl font-bold text-gray-900">Surveys</h1>
+        <router-link
+          :to="{name: 'SurveyCreate'}"
+          class="
+            py-2
+            px-3
+            text-white
+            bg-emerald-500
+            rounded-md
+            hover:bg-emerald-600
+          "
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-4 w-4 -mt-1 inline-block"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M12 4v16m8-8H4"
+            />
+          </svg>
+          Add new Survey
+        </router-link>
+      </div>
+    </template>
+
     <div class="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3">
       <div
         v-for="survey in surveys"
@@ -17,6 +51,7 @@
         <img :src="survey.image" alt="" class="w-full h-48 object-cover" />
         <h4 class="mt-4 text-lg font-bold">{{ survey.title }}</h4>
         <div v-html="survey.description" class="overflow-hidden flex-1"></div>
+
         <div class="flex justify-between items-center mt-3">
           <router-link
             :to="{ name: 'SurveyView', params: { id: survey.id } }"
@@ -49,35 +84,28 @@
             </svg>
             Edit
           </router-link>
-          <a
-            type="submit"
-            class="
-              h-12
-              w-12
-              flex
-              items-center
-              justify-center
-              rounded-full
-              border border-transparent
-              text-sm text-indigo-500
-              focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
-            "
+          <button
+          v-if="survey.id"
+          type="button"
+          @click="deleteSurvey(survey)"
+          class="
+          h-8
+          w-8
+          flex
+          items-center
+          justify-center
+          rounded-full
+          border border-transparent
+          text-sm text-red-500
+          focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-              />
-            </svg>
-          </a>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+          </svg>
+
+
+          </button>
         </div>
       </div>
     </div>
@@ -88,5 +116,12 @@
 import store from "../store";
 import { computed } from "vue";
 import PageComponent from "../components/PageComponent.vue";
+
 const surveys = computed(() => store.state.surveys);
+
+function deleteSurvey(survey){
+  if(confirm(`Are you sure you want to delete this survey? Operation can't be undone!!`)){
+
+  }
+}
 </script>
