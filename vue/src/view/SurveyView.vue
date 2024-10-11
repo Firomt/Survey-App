556bb5453dab0108566ec45aaf6c4d506408d06c
Firomt <firomsamt@gmail.com>
2024-10-11 22:40:10 +0300

    implement QuestionEditor component and dispaly question inputs

--- vue/src/view/SurveyView.vue
+++ vue/src/view/SurveyView.vue
@@ -286,6 +286,7 @@
       import { ref } from 'vue';
       import { useRoute } from 'vue-router';
       import PageComponent from '../components/PageComponent.vue';
+      import QuestionEditor from '../components/editor/QuestionEditor.vue';
       const route = useRoute();
 
       let model = ref({
