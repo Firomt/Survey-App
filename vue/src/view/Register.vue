f6d2c29b6721919a57a87d87da67ac545e71334a
Firomt <firomsamt@gmail.com>
2025-01-13 18:56:56 +0300

    show spinner animation upon click on sign up button
    -same as the animation added for the sign up

--- vue/src/view/Register.vue
+++ vue/src/view/Register.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-    <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+    <img class="mx-auto h-10 w-auto" src="https://www.svgrepo.com/show/430194/survey-rating-feedback.svg"
       alt="Your Company" />
     <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Register For Free</h2>
   </div>
@@ -55,9 +55,17 @@
       </div>
 
       <div>
-        <button type="submit"
-          class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
-          up</button>
+        <button
+          :disabled = "loading"
+          type="submit"
+          class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+          <svg v-if="loading" class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+
+            ></path>
+          </svg>
+            Sign up</button>
       </div>
     </form>
 
@@ -85,18 +93,23 @@ const user = {
   password_confirmation: ''
 };
 
+const loading = ref(false);
 const errors = ref({});
 
 function register(ev) {
   ev.preventDefault();
+
+  loading.value = true;
   store
     .dispatch('register', user)
     .then((res) => {
+      loading.value = false;
       router.push({
         name: 'Dashboard'
       })
     })
     .catch(err => {
+      loading.value = false;
       if (err.response.status === 422) {
         errors.value = err.response.data.errors
       }
